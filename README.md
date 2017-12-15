6a792c28d821b83f49cfaedee420b734869b0edb
WuJS(吴居珊/深圳) <wujs@paxsz.local>
2017-12-15 18:23:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Android studio a key international plugin, The project is for engineers working on Google play developers
+
+# How to use?([中文文档](http://blog.csdn.net/CoderYue/article/details/75177505))
+
+[Download zip](https://github.com/jinguangyue/AndroidStringsOneTabTranslation/blob/master/AndroidStringsOneTabTranslation.zip)
+ 
+![](https://github.com/jinguangyue/AndroidStringsOneTabTranslation/blob/master/screenshots/howUse.gif)
+
+Most of the code comes from: https://github.com/westlinkin/AndroidLocalizationer. 
+But it is not updated for a long time, a lot of bugs, so I changed its project, I hope everyone together to maintain.
+ 
+Differences to AndroidLocalizationer:
+ 
+ * I only joined Google Translate Because i like google. Secondly I think Google Translate is better than Microsoft Translate
+ 
+ * But Google translation need to apply api key, the key can try 365 days. You can go [google translate api](https://cloud.google.com/translate/docs/getting-started)
+ 
+ * Change the list to 3 columns to avoid blocking the language below
+ 
+ * Add all selected features
+ 
+ * I fixed some bugs. 
+ 
+ 
+ 
---
+++ src/language_engine/baidu/BaiduTranslationApi.java
@@ -0,0 +1,116 @@
+package language_engine.baidu;
+
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.intellij.ide.util.PropertiesComponent;
+import data.Key;
+import data.Log;
+import data.StorageDataKey;
+import language_engine.HttpUtils;
+import module.SupportedLanguages;
+import org.apache.http.message.BasicNameValuePair;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.URLEncoder;
+import java.util.*;
+
+public class BaiduTranslationApi {
+    private static final String TRANS_API_HOST = "http://api.fanyi.baidu.com/api/trans/vip/translate";
+
+    private String appid;
+    private String securityKey;
+
+    public BaiduTranslationApi(String appid, String securityKey) {
+        this.appid = appid;
+        this.securityKey = securityKey;
+    }
+
+    public String getTransResult(String query, String from, String to) {
+        Map<String, String> params = buildParams(query, from, to);
+        return HttpGet.get(TRANS_API_HOST, params);
+    }
+
+    private Map<String, String> buildParams(String query, String from, String to) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("q", query);
+        params.put("from", from);
+        params.put("to", to);
+
+        params.put("appid", appid);
+
+        // 随机数
+        String salt = String.valueOf(System.currentTimeMillis());
+        params.put("salt", salt);
+
+        // 签名
+        String src = appid + query + salt + securityKey; // 加密前的原文
+        params.put("sign", MD5.md5(src));
+
+        return params;
+    }
+
+    /**
+     * @param querys
+     * @param targetLanguageCode
+     * @param sourceLanguageCode
+     * @return
+     */
+    public static List<String> getTranslationJSON(@NotNull List<String> querys,
+                                                  @NotNull SupportedLanguages targetLanguageCode,
+                                                  @NotNull SupportedLanguages sourceLanguageCode) {
+        PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();
+        if (querys.isEmpty())
+            return null;
+        StringBuilder queryBuilder = new StringBuilder();
+        for (int i = 0; i < querys.size(); i++) {
+            if (i != querys.size() - 1) {
+                queryBuilder.append(URLEncoder.encode(querys.get(i))).append("\n");
+            } else if (i == querys.size() - 1) {
+                queryBuilder.append(URLEncoder.encode(querys.get(i)));
+            }
+        }
+
+        String query = queryBuilder.toString();
+        Map<String, String> params = new HashMap<>();
+        params.put("q", query);
+        params.put("from", sourceLanguageCode.getLanguageCode());
+        params.put("to", targetLanguageCode.getLanguageCode());
+        String appid = new BasicNameValuePair("client_id",
+                propertiesComponent.getValue(StorageDataKey.BingClientIdStored, Key.BAIDU_CLIENT_ID)).getValue();
+        params.put("appid", appid);
+        // 随机数
+        String salt = String.valueOf(System.currentTimeMillis());
+        params.put("salt", salt);
+
+        String securityKey = new BasicNameValuePair("client_secret",
+                propertiesComponent.getValue(StorageDataKey.BingClientSecretStored, Key.BAIDU_CLIENT_SECRET)).getValue();
+        // 签名
+        String src = appid + query + salt + securityKey; // 加密前的原文
+        params.put("sign", MD5.md5(src));
+        String getResult = HttpGet.get(TRANS_API_HOST, params);
+        if (getResult != null) {
+            JsonObject resultObj = new JsonParser().parse(getResult).getAsJsonObject();
+            JsonElement errorElement = resultObj.get("error_code");
+            if (errorElement != null) {
+                String errorCode = errorElement.getAsString();
+                String errorMsg = resultObj.get("error_msg").getAsString();
+                return null;
+            } else {
+                JsonArray translations = resultObj.getAsJsonArray("trans_result");
+                if (translations != null) {
+                    List<String> result = new ArrayList<>();
+                    for (int i = 0; i < translations.size(); i++) {
+                        result.add(translations.get(i).getAsJsonObject().get("dst").getAsString());
+                    }
+                    return result;
+                }
+            }
+        } else {
+            return null;
+        }
+        return null;
+    }
+}
